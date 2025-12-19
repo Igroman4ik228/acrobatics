c905bd120ae805346713cbf15bbd307f3c48ab9b
igroman4ik-pc <ni4000009@gmail.com>
2025-12-20 02:15:41 +0300

    fonts and fixes

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,14 @@
 import mobileNav from "./modules/mobile-nav.js";
 import contactLinks from "./modules/contact-links.js";
+import AOS from "./modules/aos.js";
 
 // Инициализация функциональности
 mobileNav();
 contactLinks();
+AOS.init({
+  duration: 800,
+  easing: "ease-in-out-quad",
+  once: false,
+  offset: 100,
+  delay: 0,
+});
